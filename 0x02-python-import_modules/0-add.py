4fa136c890c78bac4144ca24772c6d9c64b4ed42
Odili Chinedum Christian <odili.official@gmail.com>
2024-03-14 14:53:23 -0700

    modified 0-add and readme

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 if __name__=="__main__":
     from add_0 import add
 
@@ -7,4 +5,3 @@ a = 1
 b = 2
 
 print("{} + {} = {}".format(a,b, add(a,b)))
-
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # alx-higher_level_programming
 Python Higher Level Programming
+Task 0: imports funcion def add(a, b) and prints the result of the addition.
